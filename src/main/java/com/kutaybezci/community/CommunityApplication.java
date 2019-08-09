8123ed7204aec1df18d86f6e21c31e94d2b7ce7d
kutaybezci <kutay.bezci@gmail.com>
2019-08-10 02:24:48 +0300

    new file:   pom.xml 	new file:   src/main/java/com/kutaybezci/community/CommunityApplication.java 	new file:   src/main/java/com/kutaybezci/community/config/SpringSecurityConfig.java 	new file:   src/main/java/com/kutaybezci/community/controller/DefaultController.java 	new file:   src/main/resources/application.properties 	new file:   src/main/resources/messages.properties 	new file:   src/main/resources/templates/home.html 	new file:   src/main/resources/templates/login.html 	new file:   src/main/resources/templates/menu.html 	new file:   src/test/java/com/kutaybezci/community/CommunityApplicationTests.java

---
+++ src/main/java/com/kutaybezci/community/CommunityApplication.java
@@ -0,0 +1,24 @@
+package com.kutaybezci.community;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.support.ResourceBundleMessageSource;
+
+@SpringBootApplication
+public class CommunityApplication {
+
+    @Bean
+    public MessageSource messageSource() {
+        ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
+        messageSource.setBasename("messages");
+        messageSource.setDefaultEncoding("UTF-8");
+        return messageSource;
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(CommunityApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/kutaybezci/community/config/SpringSecurityConfig.java
@@ -0,0 +1,59 @@
+package com.kutaybezci.community.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+
+@Configuration
+// http://docs.spring.io/spring-boot/docs/current/reference/html/howto-security.html
+// Switch off the Spring Boot security configuration
+//@EnableWebSecurity
+public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+
+        http.csrf().disable()
+                .authorizeRequests()
+                .antMatchers("/", "/home", "/about").permitAll()
+                .antMatchers("/admin/**").hasAnyRole("ADMIN")
+                .antMatchers("/user/**").hasAnyRole("USER")
+                .anyRequest().authenticated()
+                .and()
+                .formLogin()
+                .loginPage("/login")
+                .permitAll()
+                .and()
+                .logout()
+                .permitAll();
+    }
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+
+        auth.inMemoryAuthentication()
+                .withUser("user").password("user").roles("USER")
+                .and()
+                .withUser("admin").password("admin").roles("ADMIN");
+    }
+
+    // For now development purposes
+    @SuppressWarnings("deprecation")
+    @Bean
+    public NoOpPasswordEncoder passwordEncoder() {
+        return (NoOpPasswordEncoder) NoOpPasswordEncoder.getInstance();
+    }
+
+    /*
+    //Spring Boot configured this already.
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web
+                .ignoring()
+                .antMatchers("/resources/**", "/static/**", "/css/**", "/js/**", "/images/**");
+    }*/
+}
