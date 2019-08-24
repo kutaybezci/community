0a18e139493d08e3cedaf3bd2ac87039caa85327
kutaybezci <kutay.bezci@gmail.com>
2019-08-24 14:53:55 +0300

    permissive license

--- src/main/java/com/kutaybezci/community/bl/MemberService.java
+++ src/main/java/com/kutaybezci/community/bl/MemberService.java
@@ -1,27 +1,11 @@
-/*
- * Copyright (C) 2019 kutay.bezci
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- */
 package com.kutaybezci.community.bl;
 
 import com.kutaybezci.community.types.bl.CreateMemberRequest;
 import com.kutaybezci.community.types.bl.CreateMemberResponse;
 import com.kutaybezci.community.types.bl.DisplayMemberRequest;
-import com.kutaybezci.community.types.bl.MemberDisplay;
 import com.kutaybezci.community.types.bl.ListMemberRequest;
 import com.kutaybezci.community.types.bl.ListMemberResponse;
+import com.kutaybezci.community.types.bl.MemberDisplay;
 import com.kutaybezci.community.types.bl.UpdateMemberRequest;
 import com.kutaybezci.community.types.bl.UpdateMemberResponse;
 import java.util.Optional;
--- src/main/java/com/kutaybezci/community/config/CommandLineAppStartupRunner.java
+++ src/main/java/com/kutaybezci/community/config/CommandLineAppStartupRunner.java
@@ -1,19 +1,3 @@
-/*
- * Copyright (C) 2019 kutay.bezci
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- */
 package com.kutaybezci.community.config;
 
 import com.kutaybezci.community.bl.MemberService;
@@ -29,13 +13,14 @@ import org.springframework.stereotype.Component;
 @Component
 @Slf4j
 public class CommandLineAppStartupRunner implements CommandLineRunner {
+
     @Autowired
     private MemberService memberService;
+
     @Override
-    public void run(String...args) throws Exception {
-        if(memberService.doInitAdmin()){
+    public void run(String... args) throws Exception {
+        if (memberService.doInitAdmin()) {
             log.info("Admin created");
         }
     }
 }
-
